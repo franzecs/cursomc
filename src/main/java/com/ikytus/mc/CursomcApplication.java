da3635bd4b68e10f9049b4eddf193f3a4545c83c
franzecs <franzecs@gmail.com>
2018-02-04 15:49:48 -0200

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/ikytus/mc/CursomcApplication.java
+++ src/main/java/com/ikytus/mc/CursomcApplication.java
@@ -70,6 +70,11 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
+		Categoria cat3 = new Categoria(null,"Cama e Mesa");
+		Categoria cat4 = new Categoria(null,"Jardinagem");
+		Categoria cat5 = new Categoria(null,"Mecânica");
+		Categoria cat6 = new Categoria(null,"Piscina");
+		Categoria cat7 = new Categoria(null,"Limpeza");
 		
 		Produto p1 = new Produto(null,"Computador", 2000.00);
 		Produto p2 = new Produto(null,"Impressora", 800.00);
@@ -85,7 +90,7 @@ public class CursomcApplication implements CommandLineRunner {
 		Estado est1 = new Estado(null,"Minas Gerais");
 		Estado est2 = new Estado(null,"São Paulo");
 		
-		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		categoriaRepository.save(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.save(Arrays.asList(p1,p2,p3));
 		
 		Cidade c1 = new Cidade(null,"Uberlândia", est1);
--- src/main/java/com/ikytus/mc/resources/CategoriaResource.java
+++ src/main/java/com/ikytus/mc/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ikytus.mc.domain.Categoria;
 import com.ikytus.mc.dto.CategoriaDTO;
 import com.ikytus.mc.service.CategoriaService;
-
-
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -33,6 +33,17 @@ public class CategoriaResource {
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> categorias = categoriaService.findAll();
 		List<CategoriaDTO> listDto = categorias.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0")Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome")String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC")String direction) {
+		Page<Categoria> categorias = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = categorias.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
 	
