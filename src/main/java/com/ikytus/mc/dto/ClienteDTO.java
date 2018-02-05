206dca161a838f8350e9c7ec444661e02a0f1008
franzecs <franzecs@gmail.com>
2018-02-05 08:57:53 -0200

    Validação customizada de CPF ou CNPJ em cliente

--- src/main/java/com/ikytus/mc/dto/ClienteDTO.java
+++ src/main/java/com/ikytus/mc/dto/ClienteDTO.java
@@ -6,6 +6,8 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 import com.ikytus.mc.domain.AbstractEntity;
 import com.ikytus.mc.domain.Cliente;
+import com.ikytus.mc.service.validation.ClienteInsert;
+
 
 public class ClienteDTO extends AbstractEntity{
 	private static final long serialVersionUID = 1L;
---
+++ src/main/java/com/ikytus/mc/service/validation/ClienteInsertValidator.java
@@ -0,0 +1,42 @@
+package com.ikytus.mc.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.ikytus.mc.domain.enums.TipoCliente;
+import com.ikytus.mc.dto.ClienteNewDTO;
+import com.ikytus.mc.resources.exception.FieldMessage;
+import com.ikytus.mc.service.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		if(objDto.getTipo()==null) {
+			list.add(new FieldMessage("tipo", "Tipo não pode ser nulo"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
