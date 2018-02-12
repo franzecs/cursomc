db4cc9c39c623a8ad136068f7172c9848ceb1f5a
franzecs <franzecs@gmail.com>
2018-02-12 15:31:05 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/ikytus/mc/config/DevConfig.java
+++ src/main/java/com/ikytus/mc/config/DevConfig.java
@@ -1,12 +1,15 @@
 package com.ikytus.mc.config;
 
 import java.text.ParseException;
+import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.ikytus.mc.service.DBService;
 import com.ikytus.mc.service.EmailService;
@@ -34,7 +37,26 @@ public class DevConfig {
 	}
 	
 	@Bean
+	public JavaMailSender getJavaMailSender() {
+	   
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	    mailSender.setUsername("fafor.edu@gmail.com");
+	    mailSender.setPassword("senha1234");
+	     
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "false");
+	     
+	    return mailSender;
+	}
+		
+	@Bean
 	public EmailService emailService() {
 		return new SmtpEmailService();
 	}
+	
 }
