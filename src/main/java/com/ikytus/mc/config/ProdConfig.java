3bf1f6e555bc72eda7f1565797ae8a034e19641a
franzecs <franzecs@gmail.com>
2018-02-20 08:41:53 -0300

    deploy heroku ajustes

---
+++ src/main/java/com/ikytus/mc/config/ProdConfig.java
@@ -0,0 +1,61 @@
+package com.ikytus.mc.config;
+
+import java.text.ParseException;
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import com.ikytus.mc.service.DBService;
+import com.ikytus.mc.util.email.EmailService;
+import com.ikytus.mc.util.email.JavaMailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean(name = "javaMailSender")
+	public JavaMailSender getJavaMailSender() {
+	   
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	    mailSender.setUsername("fafor.edu@gmail.com");
+	    mailSender.setPassword("senha1234");
+	     
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "false");
+	     
+	    return mailSender;
+	}
+  		
+	@Bean
+	public EmailService emailService() {
+		return new JavaMailService(getJavaMailSender());
+	}
+}
